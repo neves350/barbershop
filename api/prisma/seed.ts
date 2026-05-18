86e0bd9ebaaecbdad2f93fd69c681e22ca4f4913
neves350 <martimneves350@gmail.com>
2026-05-18 15:43:53 +0100

    feat(db): setup Prisma schema with Neon PostgreSQL
    - Configure Neon database connection
    - Create website db schema (Worker, Service, Booking and BookingService)
    - Include initial migration and client for services

---
+++ api/prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from 'src/generated/prisma/client'
+import { Services } from './services'
+
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaNeon({ connectionString })
+const prisma = new PrismaClient({ adapter, log: ['query'] })
+
+async function seed() {
+	// Create services list to the table
+	for (const service of Services) {
+		const { ...services } = service
+
+		await prisma.service.create({
+			data: {
+				name: services.name,
+				description: services.description,
+				price: services.price,
+				category: services.category,
+				duration: services.duration,
+			},
+		})
+	}
+}
+
+seed().then(() => {
+	console.log('Database seeded!')
+	prisma.$disconnect()
+})
