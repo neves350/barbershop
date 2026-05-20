a8a9596f988c6e42ee2144402dc6a0572da19790
neves350 <martimneves350@gmail.com>
2026-05-20 20:15:26 +0100

    feat(api): add ServiceModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from './prisma/prisma.module'
+import { ServiceModule } from './modules/service/service.module';
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+	imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, ServiceModule],
 	controllers: [],
 	providers: [],
 })
