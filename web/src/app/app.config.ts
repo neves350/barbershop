00a299a1f56b7bc47b1df63e43dd1c624f9587bb
neves350 <martimneves350@gmail.com>
2026-05-27 16:29:34 +0100

    chore(web): remove base-url interceptor in favour of environment config

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,16 +1,15 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { provideHttpClient } from '@angular/common/http'
 import {
 	ApplicationConfig,
 	provideBrowserGlobalErrorListeners,
 } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { routes } from './app.routes'
-import { baseUrlInterceptor } from './core/interceptors/base-url.interceptor'
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideBrowserGlobalErrorListeners(),
 		provideRouter(routes),
-		provideHttpClient(withInterceptors([baseUrlInterceptor])),
+		provideHttpClient(),
 	],
 }
